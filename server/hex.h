ca060e7afc559dad77e8a83ce01811ab56814f07
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2020-10-04 15:27:00 +0300

    Simplify hex color and byte getters

--- server/hex.h
+++ server/hex.h
@@ -10,19 +10,17 @@ uint32_t getHexDigit(uint32_t result, uint8_t digit) {
   return (result << 4) + nextInt;
 }
 
-uint32_t getHexColor(int &position, uint8_t line[]) {
+uint32_t getHexColor(int &pos, uint8_t line[]) {
   uint32_t result = 0;
   for (int i = 0; i < 6; i++) {
-    result = getHexDigit(result, line[position + i]);
+    result = getHexDigit(result, line[pos++]);
   }
-  position += 6;
   return result;
 }
 
-unsigned char getHexByte(int &position, uint8_t line[]) {
-  uint8_t result = getHexDigit(0, line[position]);
-  result = getHexDigit(result, line[position + 1]);
-  position += 2;
+unsigned char getHexByte(int &pos, uint8_t line[]) {
+  uint8_t result = getHexDigit(0, line[pos++]);
+  result = getHexDigit(result, line[pos++]);
   return result;
 }
 
